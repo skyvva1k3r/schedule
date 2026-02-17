2bfc3aeaf9f3a64b0797af6ac435a8c77e95069d
skyLaptop <antargtig@inbox.ru>
2026-02-17 15:57:45 +0300

    new funcs

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 import random
+from datetime import date
+from typing import Optional
 import bcrypt
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Header, Depends
+from fastapi.responses import FileResponse
 from jose import jwt
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy import MetaData, create_engine, text
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose.exceptions import JWTError
-
 bearer = HTTPBearer()
 load_dotenv()
 password = os.getenv("password")
@@ -34,6 +36,7 @@ class UserRegistration(BaseModel):
     password: str
     role: str
     name: str
+    identification : Optional[int] = None
 
 class UserVerification(BaseModel):
     login: str
@@ -46,16 +49,32 @@ class UserLogin(BaseModel):
 class Lesson(BaseModel):
     item_id : int
     group_id : int
-    date : str
+    date : date
     lesson_number : int
 
-class Grade(BaseModel):
+class SetGrade(BaseModel):
     item_id : int
     group_id : int
-    date : str
+    date : date
     lesson_number : int
-    grade : int
+    grade: int = Field(ge=1, le=5)
     student_id : int
+
+class DelGrade(BaseModel):
+    item_id : int
+    group_id : int
+    date : date
+    lesson_number : int
+    student_id : int 
+
+class ChangePass(BaseModel):
+    login: str
+    new_password: str
+
+@app.get("/")
+async def index():
+    return FileResponse('index_schedule.html')
+
 
 @app.post('/reg')
 async def registration(user : UserRegistration):
@@ -74,8 +93,14 @@ async def registration(user : UserRegistration):
         with engine.connect() as connection:
             h = h.decode('utf-8')
             code = random.randrange(100, 999)
-            sql = text("INSERT INTO public.users(login, role, password, code, name) VALUES (:login, :role, :password, :code, :name);")
-            result = connection.execute(sql, {"login": user.login, "role" : user.role, "password" : h, "code" : code, "name" : user.name})
+            if user.role != "teacher":
+                sql = text("INSERT INTO public.users(login, role, password, code, name) VALUES (:login, :role, :password, :code, :name);")
+                result = connection.execute(sql, {"login": user.login, "role" : user.role, "password" : h, "code" : code, "name" : user.name})
+            else:
+                if user.identification == None:
+                    return {"status" : "info", "message" : "No identification given."}
+                sql = text("INSERT INTO public.users(login, role, password, code, name, teacher_id) VALUES (:login, :role, :password, :code, :name, :identification);")
+                result = connection.execute(sql, {"login": user.login, "role" : user.role, "password" : h, "code" : code, "name" : user.name, "identification" : user.identification})
             connection.commit()
         return {"status" : "success", "message" : "Account successfully created. Verification required."}
     
@@ -148,12 +173,12 @@ async def grades(lesson : Lesson, credentials: HTTPAuthorizationCredentials = De
         rows = result.fetchall()
     if rows:
         rows = [tuple(r) for r in rows]
-        return {"status" : "success", "message" : "Schedule fetched successfully.", "schedule" : rows}
+        return {"status" : "success", "message" : "Grades fetched successfully.", "schedule" : rows}
     else:
         return {"status" : "info", "message" : "No schedule found."}
 
-@app.post('/schedule/lesson/grade')
-async def set_grade(grade : Grade, credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+@app.post('/schedule/lesson/set_grade')
+async def set_grade(grade : SetGrade, credentials: HTTPAuthorizationCredentials = Depends(bearer)):
     token = credentials.credentials
     try:
         decoded = jwt.decode(token, secret, algorithms=["HS256"])
@@ -166,8 +191,153 @@ async def set_grade(grade : Grade, credentials: HTTPAuthorizationCredentials = D
         rows = result.fetchall()
         if rows[0][2] == "student":
             return {"status" : "info", "message" : "You cant do that."}
-    with engine.connect() as connection:
+        if rows[0][2] == "teacher":
+            sql = text("SELECT * FROM items WHERE teacher_id = :teacher_id and id = :item_id")
+            result = connection.execute(sql, {"teacher_id": rows[0][7], "item_id": grade.item_id})
+            rows = result.fetchall()
+            if not rows:
+                return {"status": "info", "message": "Not your item to deal with grades."}
+            sql = text("SELECT * FROM students WHERE id = :student_id AND group_id = :group_id")
+            result = connection.execute(sql, {"student_id": grade.student_id, "group_id": grade.group_id})
+            student = result.fetchall()
+            if not student:
+                return {"status": "info", "message": "Student not found in this group."}
         sql = text("INSERT INTO public.grades(grade, student_id, item_id, group_id, date, lesson_number) VALUES (:grade, :student_id, :item_id, :group_id, :date, :lesson_number);")
         result = connection.execute(sql, {"grade" : grade.grade, "student_id" : grade.student_id, "item_id" : grade.item_id, "group_id" : grade.group_id, "date" : grade.date, "lesson_number" : grade.lesson_number})
         connection.commit()
         return {"status" : "success", "message" : "Grade added successfully."}
+
+@app.post('/schedule/lesson/del_grade')
+async def del_grade(grade : DelGrade, credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+    token = credentials.credentials
+    try:
+        decoded = jwt.decode(token, secret, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = decoded["id"]
+    with engine.connect() as connection:
+        sql = text("SELECT * FROM users WHERE id = :id")
+        result = connection.execute(sql, {"id" : user_id})
+        rows = result.fetchall()
+        if rows[0][2] == "student":
+            return {"status" : "info", "message" : "You cant do that."}
+        if rows[0][2] == "teacher":
+            sql = text("SELECT * FROM items WHERE teacher_id = :teacher_id and id = :item_id")
+            result = connection.execute(sql, {"teacher_id": rows[0][7], "item_id": grade.item_id})
+            rows = result.fetchall()
+            if not rows:
+                return {"status": "info", "message": "Not your item to deal with grades."}
+
+            sql = text("SELECT * FROM students WHERE id = :student_id AND group_id = :group_id")
+            result = connection.execute(sql, {"student_id": grade.student_id, "group_id": grade.group_id})
+            student = result.fetchall()
+            if not student:
+                return {"status": "info", "message": "Student not found in this group."}  
+        sql = text("""SELECT * FROM grades 
+                    WHERE item_id = :item_id AND group_id = :group_id 
+                    AND lesson_number = :lesson_number AND date = :date 
+                    AND student_id = :student_id""")
+        result = connection.execute(sql, {
+            "item_id": grade.item_id, "group_id": grade.group_id,
+            "lesson_number": grade.lesson_number, "date": grade.date,
+            "student_id": grade.student_id
+        })
+        if not result.fetchall():
+            return {"status": "info", "message": "Grade not found."}  
+        sql = text("DELETE from grades WHERE item_id = :item_id and group_id = :group_id and lesson_number = :lesson_number and date = :date and student_id = :student_id")
+        result = connection.execute(sql, {"lesson_number" : grade.lesson_number, "item_id" : grade.item_id, "group_id" : grade.group_id, "date" : grade.date, "student_id" : grade.student_id})
+        connection.commit()
+        return {"status" : "success", "message" : "Grade was successfully deleted."}
+    
+@app.post('/schedule/lesson/change_grade')
+async def change_grade(grade : SetGrade, credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+    token = credentials.credentials
+    try:
+        decoded = jwt.decode(token, secret, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = decoded["id"]
+    with engine.connect() as connection:
+        sql = text("SELECT * FROM users WHERE id = :id")
+        result = connection.execute(sql, {"id" : user_id})
+        rows = result.fetchall()
+        if rows[0][2] == "student":
+            return {"status" : "info", "message" : "You cant do that."}
+        if rows[0][2] == "teacher":
+            sql = text("SELECT * FROM items WHERE teacher_id = :teacher_id and id = :item_id")
+            result = connection.execute(sql, {"teacher_id": rows[0][7], "item_id": grade.item_id})
+            rows = result.fetchall()
+            if not rows:
+                return {"status": "info", "message": "Not your item to deal with grades."}
+
+            sql = text("SELECT * FROM students WHERE id = :student_id AND group_id = :group_id")
+            result = connection.execute(sql, {"student_id": grade.student_id, "group_id": grade.group_id})
+            student = result.fetchall()
+            if not student:
+                return {"status": "info", "message": "Student not found in this group."}
+        sql = text("""SELECT * FROM grades 
+                    WHERE item_id = :item_id AND group_id = :group_id 
+                    AND lesson_number = :lesson_number AND date = :date 
+                    AND student_id = :student_id""")
+        result = connection.execute(sql, {
+            "item_id": grade.item_id, "group_id": grade.group_id,
+            "lesson_number": grade.lesson_number, "date": grade.date,
+            "student_id": grade.student_id
+        })
+        if not result.fetchall():
+            return {"status": "info", "message": "Grade not found."}
+        sql = text("UPDATE grades SET grade = :grade WHERE item_id = :item_id and group_id = :group_id and lesson_number = :lesson_number and date = :date and student_id = :student_id")
+        result = connection.execute(sql, {"grade" : grade.grade, "lesson_number" : grade.lesson_number, "item_id" : grade.item_id, "group_id" : grade.group_id, "date" : grade.date, "student_id" : grade.student_id})
+        connection.commit()
+        return {"status" : "success", "message" : "Grade was successfully changed."}
+    
+@app.get("/adminpanel")
+async def admin_panel(credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+    token = credentials.credentials
+    try:
+        decoded = jwt.decode(token, secret, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = decoded["id"]
+    with engine.connect() as connection:
+        sql = text("SELECT * FROM users WHERE id = :id")
+        result = connection.execute(sql, {"id" : user_id})
+        rows = result.fetchall()
+        if rows[0][2] != "admin":
+            return {"status" : "info", "message" : "You cant do that."}
+        sql = text("SELECT * FROM users")
+        result = connection.execute(sql)
+        rows = result.fetchall()
+        users_list = []
+        for user in rows:
+            user = list(user)
+            user[4] = "***"
+            users_list.append(user)
+        return {"status": "success", "message": "Successfully fetched all user data.", "users": users_list}
+
+@app.post("/adminpanel/changepassword")
+async def change_password(user: ChangePass, credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+    token = credentials.credentials
+    try:
+        decoded = jwt.decode(token, secret, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = decoded["id"]
+    with engine.connect() as connection:
+        sql = text("SELECT * FROM users WHERE id = :id")
+        result = connection.execute(sql, {"id": user_id})
+        rows = result.fetchall()
+        if rows[0][2] != "admin":
+            return {"status": "info", "message": "You cant do that."}
+
+        sql = text("SELECT * FROM users WHERE login = :login")
+        result = connection.execute(sql, {"login": user.login})
+        target = result.fetchall()
+        if not target:
+            return {"status": "info", "message": "User not found."}
+
+        new_hash = bcrypt.hashpw(user.new_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        sql = text("UPDATE users SET password = :password WHERE login = :login")
+        connection.execute(sql, {"password": new_hash, "login": user.login})
+        connection.commit()
+    return {"status": "success", "message": "Password changed successfully."}
